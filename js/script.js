d78c7f428d1a0b11c693329e1028f8b975694bc3
Krimsi <astt509@mail.ru>
2018-10-15 15:45:27 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,12 @@
+﻿
+$("q1").css("fontSize","33px");
+$("q2").css("fontSize","28px");
+console.log('sdfgsdg');
+
+$(document).ready(function(){
+	console.log('ready');
+
+   $("#but1").click(function(){ $('#square').hide(3000)});
+   $("#but2").click(function(){ $('#square').show(3000)});
+ 
+})
